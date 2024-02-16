e68da8e3f7804a3780a3277612cfac775f140731
NitinNSB <98605848+NitinNSB@users.noreply.github.com>
2024-02-16 22:46:34 +0530

    Delete DOM directory

--- DOM/index.js
+++
@@ -1,6 +0,0 @@
-var val = confirm("Changing color to red");
-if (val == true) {
-    document.querySelector("h1").style.color = "red";
-}else{
-    document.querySelector("h1").style.color = "yellow";
-}
