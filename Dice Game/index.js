c953d7b24c4055b190672f148fff4506383f1175
NitinNSB <98605848+NitinNSB@users.noreply.github.com>
2024-02-16 22:41:24 +0530

    Add files via upload

---
+++ Dice Game/index.js
@@ -0,0 +1,44 @@
+let randomNumber1 = Math.floor(Math.random()*6)+1;
+let randomNumber2 = Math.floor(Math.random()*6)+1;
+
+//Dice 1
+if(randomNumber1==1){
+    document.querySelector(".img1").setAttribute("src","./images/dice1.png");
+}else if(randomNumber1==2){
+    document.querySelector(".img1").setAttribute("src","./images/dice2.png");
+}else if(randomNumber1==3){
+    document.querySelector(".img1").setAttribute("src","./images/dice3.png");
+}else if(randomNumber1==4){
+    document.querySelector(".img1").setAttribute("src","./images/dice4.png");
+}else if(randomNumber1==5){
+    document.querySelector(".img1").setAttribute("src","./images/dice5.png");
+}else if(randomNumber1==6){
+    document.querySelector(".img1").setAttribute("src","./images/dice6.png");
+}
+//Dice 2
+
+if(randomNumber2==1){
+    document.querySelector(".img2").setAttribute("src","./images/dice1.png");
+}else if(randomNumber2==2){
+    document.querySelector(".img2").setAttribute("src","./images/dice2.png");
+}else if(randomNumber2==3){
+    document.querySelector(".img2").setAttribute("src","./images/dice3.png");
+}else if(randomNumber2==4){
+    document.querySelector(".img2").setAttribute("src","./images/dice4.png");
+}else if(randomNumber2==5){
+    document.querySelector(".img2").setAttribute("src","./images/dice5.png");
+}else if(randomNumber2==6){
+    document.querySelector(".img2").setAttribute("src","./images/dice6.png");
+}
+
+// wins and Draw
+if(randomNumber1>randomNumber2){
+    document.querySelector(".container h1").innerHTML="Player 1 Wins <span></span>&#128509;";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector(".container h1").innerHTML="Player 2 Wins &#128509;";
+}
+else{
+    document.querySelector(".container h1").innerHTML="Draw &#128517;";
+}
+
